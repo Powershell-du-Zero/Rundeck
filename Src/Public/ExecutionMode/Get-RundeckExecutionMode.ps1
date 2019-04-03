c69261e71e7063fc6e87e532ab836f0d5168e2e9
Thomas ILLIET <contact@thomas-illiet.fr>
2019-04-03 23:40:10 +0200

    ✨ Add public function Get-RundeckExecutionMode

---
+++ Src/Public/ExecutionMode/Get-RundeckExecutionMode.ps1
@@ -0,0 +1,52 @@
+function Get-RundeckExecutionMode
+{
+
+    <#
+        .SYNOPSIS
+        n/a
+
+        .PARAMETER Path
+        n/a
+
+        .EXAMPLE
+        PS C:\> Get-RundeckExecutionMode
+
+        .OUTPUTS
+        System.Boolean
+
+        .COMPONENT
+        Rundeck API
+
+        .NOTES
+        - File Name : Get-RundeckExecutionMode.ps1
+        - Author    : Thomas ILLIET
+    #>
+
+    [CmdletBinding()]
+    [OutputType( [System.Boolean] )]
+    Param()
+
+    begin
+    {
+        $functionName = $MyInvocation.MyCommand.Name
+        Write-Verbose "[${functionName}] Function started"
+    }
+
+    process
+    {
+        try
+        {
+            $systemInfo = Get-RundeckSystemInfo -ErrorAction Stop
+            return [System.Boolean]$systemInfo.System.Executions.ExecutionMode.Active
+        }
+        catch
+        {
+            Write-Error "Could not get the rundeck execution mode"
+        }
+    }
+
+    end
+    {
+        Write-Verbose "[${functionName}] Complete"
+    }
+}
